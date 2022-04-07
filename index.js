2b6da1608a674430b9f95838f43544e4913d2071
sandyu732 <47005009+sandyu732@users.noreply.github.com>
2022-04-07 13:36:48 +0530

    Update index.js

--- index.js
+++ index.js
@@ -14,5 +14,5 @@ const server = http.createServer((req, res) => {
 });
 
 server.listen(port, hostname, () => {
-    console.log(`Server running at http://${hostname}:${port}/`);
+    console.log(`Server running at the http://${hostname}:${port}/`);
 });
